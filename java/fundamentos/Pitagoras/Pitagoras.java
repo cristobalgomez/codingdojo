427e243c15c97c7450abdbea0ee95fedf44dfaff
cristóbal <criistobal@gmail.com>
2021-06-04 18:56:56 -0400

    java fundamentos

---
+++ java/fundamentos/Pitagoras/Pitagoras.java
@@ -0,0 +1,6 @@
+import java.lang.Math;
+public class Pitagoras {
+    public double calcularHipotenusa(int catetoA, int catetoB) {
+        return Math.sqrt(catetoA*catetoA + catetoB*catetoB);
+    }
+}
---
+++ java/fundamentos/Pitagoras/PitagorasTest.java
@@ -0,0 +1,8 @@
+public class PitagorasTest {
+    public static void main(String[] args) {
+        Pitagoras triangulo = new Pitagoras();
+        double area = triangulo.calcularHipotenusa(6, 10);
+
+        System.out.println(area);
+    }
+}
