fbfa36c8ec62fb0c8e472a7d2843ee976c33cb54
cristóbal <criistobal@gmail.com>
2021-06-04 11:26:04 -0400

    java fundamentos

---
+++ java/fundamentos/PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es Cristobal");
+        System.out.println("Tengo 27 anios");
+        System.out.println("Estoy viviendo en Valdivia");
+    }
+}
