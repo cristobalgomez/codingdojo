65188b8e87aa514e354dbc7b086d4e8375298a2a
cristóbal <criistobal@gmail.com>
2021-07-04 21:20:15 -0400

    java - springboot

---
+++ java/springboot/data-I/MostrarEliminarPlantilla/src/main/java/com/codingdojo/mvc/controllers/BookController.java
@@ -0,0 +1,62 @@
+package com.codingdojo.mvc.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.codingdojo.mvc.models.Book;
+import com.codingdojo.mvc.services.BookService;
+
+@Controller
+public class BookController {
+	private final BookService bookService;
+
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	@RequestMapping("/books")
+	public String index(Model model) {
+		List<Book> books = bookService.allBooks();
+		model.addAttribute("books", books);
+		return "/books/index.jsp";
+	}
+	
+	@RequestMapping("/books/new")
+	public String newBook(@ModelAttribute("book") Book book) {
+		return "/books/new.jsp";
+	}
+	
+    @RequestMapping(value="/books", method=RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/books/new.jsp";
+        } else {
+            bookService.createBook(book);
+            return "redirect:/books";
+        }
+    }
+    
+    @RequestMapping("/books/{id}")
+    public String showBook(@PathVariable("id") Long id, Model model) {
+    	Book book = bookService.findBook(id);
+    	model.addAttribute("book", book);
+    	return "/books/show.jsp";
+    }
+    
+    @RequestMapping(value="/books/{id}", method=RequestMethod.POST)
+    public String deleteBook(@PathVariable("id") Long id, @RequestParam("_method") String method) {
+    	if(method.equals("delete")) bookService.deleteBook(id);
+    	return "redirect:/books";
+    }
+    
+}
