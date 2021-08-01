a666d5f159052f52ee4df80d7f551d7bafb72be5
cristóbal <criistobal@gmail.com>
2021-08-01 17:56:23 -0400

    java - springboot

---
+++ java/springboot/belt-practica/beltexameTres/src/main/java/com/codingdojo/belt/BeltexamenApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.belt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeltexamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeltexamenApplication.class, args);
+	}
+
+}
